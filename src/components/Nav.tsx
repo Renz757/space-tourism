6e5252447632d1e3abc42e5ed5d25155a069639f
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2023-12-05 18:59:15 -0500

    color adjustments

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
 const Nav = () => {
     return (
         <>
-            <nav className='flex justify-between mt-10 items-center relative'>
+            <nav className='flex justify-between mt-10 items-center fixed z-20 w-full'>
                 {/* logo */}
                 <div className="ml-14">
                     <img src='../../assets/shared/logo.svg' />
@@ -13,14 +13,14 @@ const Nav = () => {
                 </div>
                 {/* nav items */}
                 <ul className='
-                    flex items-center space-x-8
-                    text-white text-lg font-barlow px-64
-                    bg-tropicalBlue bg-opacity-5
+                    flex items-center space-x-12
+                    text-white text-lg font-barlow px-56
+                    bg-[#979797] bg-opacity-10 backdrop-blur
                 '>
-                    <li className="py-8 cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75"><span className="font-barlow font-bold">00</span> CREW</li>
-                    <li className="py-8 cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75"><span className="font-barlow font-bold">01</span> HOME</li>
-                    <li className="py-8 cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75"><span className="font-barlow font-bold">02</span> DESTINATION</li>
-                    <li className="py-8 cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75"><span className="font-barlow font-bold">03</span> TECHNOLOGY</li>
+                    <li className="py-8 cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75"><span className="font-barlow font-bold mr-2">00</span> HOME</li>
+                    <li className="py-8 cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75"><span className="font-barlow font-bold mr-2">01</span> CREW</li>
+                    <li className="py-8 cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75"><span className="font-barlow font-bold mr-2">02</span> DESTINATION</li>
+                    <li className="py-8 cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75"><span className="font-barlow font-bold mr-2">03</span> TECHNOLOGY</li>
                 </ul>
             </nav>
         </>
