6d01a74c079a9fb69835ada434f9fe5cd4e1c6ae
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2023-12-05 20:02:50 -0500

    added BrowserRouter, Routes, and Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Nav from './components/Nav'
 import Home from './Home'
 
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-      <Nav />
-      <Home />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route index path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
