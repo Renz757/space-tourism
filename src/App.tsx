88a164ec68727876ddad3210f4b3bf85e5cb4b2b
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2023-12-06 18:29:19 -0500

    added pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Nav from './components/Nav'
-import Home from './Home'
+import Home from './pages/Home'
 
 import './App.css'
 
