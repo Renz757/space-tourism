08c449671aec0f9b90883c1c3b58c06fbf36f5d4
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2023-12-18 20:24:39 -0500

    link styling

--- src/components/DestinationNav.tsx
+++ src/components/DestinationNav.tsx
@@ -3,10 +3,10 @@ const DestinationNav = () => {
     <>
         <nav>
             <ul className="text-white space-x-5 tracking-widest font-barlow flex">
-                <li>MOON</li>
-                <li>MARS</li>
-                <li>EUROPA</li>
-                <li>TITAN</li>
+                <li className="cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">MOON</li>
+                <li className="cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">MARS</li>
+                <li className="cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">EUROPA</li>
+                <li className="cursor-pointer border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">TITAN</li>
             </ul>
         </nav>
     </>
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -15,16 +15,16 @@ const NavItems: React.FC<NavItemProps> = ({isOpen, handleToggle}) => {
     return (
         <>
 
-            <li onClick={handleClick} className="md:py-8 tracking-widest cursor-pointer border-r-4 md:border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">
+            <li onClick={handleClick} className="md:py-8 tracking-widest cursor-pointer border-r-4 md:border-r-0 md:border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">
                 <Link to="/"><span className="md:hidden lg:inline-block font-bold tracking-widest mr-2">00</span> HOME</Link>
             </li>
-            <li onClick={handleClick} className="md:py-8 tracking-widest cursor-pointer border-r-4 md:border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">
+            <li onClick={handleClick} className="md:py-8 tracking-widest cursor-pointer border-r-4 md:border-r-0 md:border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">
                 <Link to="/destination"><span className="md:hidden lg:inline-block font-bold tracking-widest mr-2">01</span> DESTINATION</Link>
             </li>
-            <li onClick={handleClick} className="md:py-8 tracking-widest cursor-pointer border-r-4 md:border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">
+            <li onClick={handleClick} className="md:py-8 tracking-widest cursor-pointer border-r-4 md:border-r-0 md:border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">
                 <Link to="/crew"><span className="md:hidden lg:inline-block font-bold tracking-widest mr-2">02</span> CREW</Link>
             </li>
-            <li onClick={handleClick} className="md:py-8 tracking-widest cursor-pointer border-r-4 md:border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">
+            <li onClick={handleClick} className="md:py-8 tracking-widest cursor-pointer border-r-4 md:border-r-0 md:border-b-4 border-tropicalBlue border-opacity-0 hover:border-white transition ease-in-out delay-75">
                 <Link to="/technology"><span className="md:hidden lg:inline-block font-bold tracking-widest mr-2">03</span> TECHNOLOGY</Link>
             </li>
         </>
